21fdb71d350823622c78e1633b2932821d2502a2
Surya Chandra Kukunuri <sckukunuri2020@gmail.com>
2024-10-30 16:50:57 +0530

    updated mongodb uri from env

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const contactRoutes = require('./routes/contacts'); // Import your routes
+require('dotenv').config()
 
+const url = process.env.MONGODB_URI
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(
+    cors({
+      origin: 'https://surya-kukunuri.com',
+      optionsSuccessStatus: 200,
+    })
+  );
+app.use(bodyParser.json()); 
 
-// Connect to MongoDB Atlas
-const mongoURI = 'mongodb+srv://suryach24:F8SNjwVgs9IGjGWr@cluster0.tyycl.mongodb.net/address-book?retryWrites=true&w=majority'; 
-
-mongoose.connect(mongoURI)
+mongoose.connect(url, {})
     .then(() => console.log('MongoDB Atlas connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
